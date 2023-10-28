26fb19e1883c0fc36397b67fb8d48cca7fd9f8a3
LauraLopezLeon <laura.lopez1998@gmail.com>
2023-10-28 12:50:06 -0500

    Puzzle
    Ya funciona al menos que la imagen esté limitada en un espacio y cambié todo a un slider

--- projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_1.cs
+++ projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_1.cs
@@ -1,34 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+
 public class Puzzle_1 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    private RectTransform _rectTransform;
-    private Image _image;
+    //Rompecabezas
+    [SerializeField] private GameObject _pieza_rompecabezas;
+    private Vector3 movimiento_pieza;
+
+    //Scroll
+    [SerializeField] private Slider _scroll;
     private Vector3 movimiento;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        _image.color = new Color32(255,255,255,170);
+        _pieza_rompecabezas.GetComponent<Image>().color = new Color32(255, 255, 255, 170);
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        //_rectTransform.anchoredPosition += eventData.delta;
-        movimiento = new Vector3(Input.mousePosition.x, transform.position.y ,0f);
-        transform.position = movimiento;
-    }
+    //public void OnDrag(PointerEventData eventData)
+    //{
+    //    if (_scroll.value >= 0 && _scroll.value <= 1)
+    //    {
+    //        movimiento_pieza = new Vector3(Input.mousePosition.x, _pieza_rompecabezas.transform.position.y, 0f);
+    //        _pieza_rompecabezas.transform.position = movimiento_pieza;
+    //    }
+    //}
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        _image.color = new Color(255, 255, 255, 170);
+        _pieza_rompecabezas.GetComponent<Image>().color = new Color(255, 255, 255, 225);
     }
-
     void Start()
     {
-        _rectTransform = GetComponent<RectTransform>();
-        _image = GetComponent<Image>();
+        //OnSliderValueChanged(_scroll.value);
+    }
+    //private void Update()
+    //{
+    //    OnSliderValueChanged(_scroll.value);
+    //}
+    //public void OnSliderValueChanged(float value)
+    //{
+    //    if (value >= 0 && value <= 10070)
+    //    {
+    //        movimiento_pieza = new Vector3(value, _pieza_rompecabezas.transform.position.y, 0f);
+    //        _pieza_rompecabezas.transform.position = movimiento_pieza;
+    //    }
+    //}
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (_scroll.value > 0 && _scroll.value < 1)
+        {
+            movimiento_pieza = new Vector3(Input.mousePosition.x, _pieza_rompecabezas.transform.position.y, 0f);
+            _pieza_rompecabezas.transform.position = movimiento_pieza;
+        }
     }
 }
