368a3c5ac74d339b4b10470bda28d6a1b5ca9c95
LauraLopezLeon <laura.lopez1998@gmail.com>
2023-10-28 13:34:55 -0500

    Avance
    Funciona en parte, falta el final

--- projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_1.cs
+++ projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_1.cs
@@ -12,6 +12,9 @@ public class Puzzle_1 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
     [SerializeField] private GameObject _pieza_rompecabezas;
     private Vector3 movimiento_pieza;
 
+    //Sombra
+    [SerializeField] private GameObject _pieza_faltante;
+    public float distancia_exitosa;
     //Scroll
     [SerializeField] private Slider _scroll;
     private Vector3 movimiento;
@@ -21,35 +24,20 @@ public class Puzzle_1 : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDrag
         _pieza_rompecabezas.GetComponent<Image>().color = new Color32(255, 255, 255, 170);
     }
 
-    //public void OnDrag(PointerEventData eventData)
-    //{
-    //    if (_scroll.value >= 0 && _scroll.value <= 1)
-    //    {
-    //        movimiento_pieza = new Vector3(Input.mousePosition.x, _pieza_rompecabezas.transform.position.y, 0f);
-    //        _pieza_rompecabezas.transform.position = movimiento_pieza;
-    //    }
-    //}
-
     public void OnEndDrag(PointerEventData eventData)
     {
         _pieza_rompecabezas.GetComponent<Image>().color = new Color(255, 255, 255, 225);
+
+        float distancia = (_pieza_rompecabezas.transform.position - _pieza_faltante.transform.position).magnitude;
+        if (distancia <= distancia_exitosa)
+        {
+            Debug.Log("Bien hecho perra");
+        }
     }
     void Start()
     {
-        //OnSliderValueChanged(_scroll.value);
+
     }
-    //private void Update()
-    //{
-    //    OnSliderValueChanged(_scroll.value);
-    //}
-    //public void OnSliderValueChanged(float value)
-    //{
-    //    if (value >= 0 && value <= 10070)
-    //    {
-    //        movimiento_pieza = new Vector3(value, _pieza_rompecabezas.transform.position.y, 0f);
-    //        _pieza_rompecabezas.transform.position = movimiento_pieza;
-    //    }
-    //}
     public void OnDrag(PointerEventData eventData)
     {
         if (_scroll.value > 0 && _scroll.value < 1)
