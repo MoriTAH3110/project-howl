582a0c02efa72659090752f4d2fece96260769e9
LauraLopezLeon <laura.lopez1998@gmail.com>
2023-11-11 10:05:04 -0500

    Avance
    Mini avance en programación

--- projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_3.cs
+++ projectHowl/Assets/Code/Scripts/Puzzles/Puzzle_3.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puzzle_3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button[] buttons; // Botones asignados desde el editor
+    private int dificultadSecuencia = 5;
+    private int inicioSecuencia = 0;
+    private int[] sequence;
+    private int sequenceIndex;
+    private bool playerTurn;
+
     void Start()
     {
-        
+        sequence = new int[6]; // Inicializar la secuencia con el tamaño adecuado
+        RellenarSecuenciaDeArray();
+        StartCoroutine(IluminarBotonSecuencia());
+        Debug.Log("entra2");
     }
-
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    void RellenarSecuenciaDeArray()
+    {
+        // Generar una nueva secuencia
+        for (int i = inicioSecuencia; i < dificultadSecuencia; i++)
+        {
+            sequence[i] = Random.Range(0, dificultadSecuencia); // Generar números para los botones
+        }
+    }
+    IEnumerator IluminarBotonSecuencia()
+    {
+        for (int i = inicioSecuencia; i < dificultadSecuencia; i++)
+        {
+            Debug.Log("entra1");
+            yield return new WaitForSeconds(1.0f); // Tiempo entre cada botón iluminado
+            int buttonIndex = sequence[i];
+            Button buttonToHighlight = buttons[buttonIndex];
+            ColorBlock colors = buttonToHighlight.colors;
+            colors.normalColor = Color.yellow; // Cambiar el color a amarillo para simular iluminación
+            buttonToHighlight.colors = colors;
+            yield return new WaitForSeconds(0.5f);
+            colors.normalColor = Color.white; // Cambiar de vuelta al color original
+            buttonToHighlight.colors = colors;
+        }
     }
 }
