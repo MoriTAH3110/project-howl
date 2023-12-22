40ca289af0544072519f8aed862232d897b236cd
MoriTAH3110 <dany.mobe3110@gmail.com>
2023-12-22 15:35:57 -0500

    Moving stairs set up

--- projectHowl/Assets/Code/Scripts/Gameplay/Platforms/MovingPlatform.cs
+++ projectHowl/Assets/Code/Scripts/Gameplay/Platforms/MovingPlatform.cs
@@ -8,14 +8,60 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField]
     private float _speed = 5.0f;
 
-    [SerializeField]
-    private int _waypointIndex = 0;
+    
+    private int _nextWaypointIndex = 0;
+    private int _prevWaypointIndex = 0;
+    private float _timeToWaypoint;
+    private float _elapsedTime;
 
     void FixedUpdate() {
         Travel();
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void Start() {
+        SetTimeToWaypoint();
+    }
+
+    private void Travel() {
+        if (waypoints.Length <= 0) {
+            return;
+        }
+
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = (_elapsedTime / _timeToWaypoint);
+        this.transform.position = Vector3.Lerp(GetWaypointPosition(_prevWaypointIndex), GetWaypointPosition(_nextWaypointIndex), elapsedPercentage);
+
+        if (Vector3.Distance(transform.position, GetWaypointPosition(_nextWaypointIndex)) < 0.1f) {
+            SetWaypointsIndexes();
+            SetTimeToWaypoint();
+        }
+    }
+
+    private void SetWaypointsIndexes() {
+        _prevWaypointIndex = _nextWaypointIndex;
+
+        if ( _nextWaypointIndex < (waypoints.Length - 1 )) {
+            _nextWaypointIndex += 1;
+        } else {
+            _nextWaypointIndex = 0;
+        }
+    }
+
+    private void SetTimeToWaypoint() {
+        _elapsedTime = 0f;
+
+        float distanceToNextWaypoint = Vector3.Distance(this.transform.position, GetWaypointPosition(_nextWaypointIndex));
+
+        _timeToWaypoint = distanceToNextWaypoint / _speed;
+    }
+
+    private Vector3 GetWaypointPosition(int waypointIndex) {
+        return waypoints[waypointIndex].transform.position;
+    }
+
+    //Move player with platform
+      private void OnTriggerEnter(Collider other) {
         if(other.tag == "Player") {
             other.transform.SetParent(this.transform);
         }
@@ -24,28 +70,6 @@ public class MovingPlatform : MonoBehaviour
     private void OnTriggerExit(Collider other) {
         if(other.tag == "Player") {
             other.transform.SetParent(null);
-        }
-    }
-
-    private void Travel() {
-        if (waypoints.Length <= 0) {
-            return;
-        }
-
-        Vector3 moveDirection = waypoints[_waypointIndex].position - transform.position;
-
-        transform.Translate(moveDirection * _speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, waypoints[_waypointIndex].position) < 0.1f) {
-            NextWaypointIndex();
-        }
-    }
-
-    private void NextWaypointIndex() {
-        if ( _waypointIndex < (waypoints.Length - 1 )) {
-            _waypointIndex += 1;
-        } else {
-            _waypointIndex = 0;
         }
     }
 }
